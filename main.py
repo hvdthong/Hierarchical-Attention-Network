49193f57e201bbff2d95f7c7fbed182f38add74a
Thong Hoang <hvdthong@Thongs-MacBook-Pro.local>
2019-02-06 13:53:45 +0800

    add files

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ import string
 
 import itertools
 import more_itertools
-
+import random
 
 ## calculate frequency of words
 from collections import defaultdict
@@ -129,7 +129,7 @@ class SentenceRNN(nn.Module):
                 else:
                     r.append(inp[j][i])
             r1 = np.asarray([sub_list + [0] * (max_seq_len - len(sub_list)) for sub_list in r])
-            _s, state_word = self.wordRNN(torch.cuda.LongTensor(r1).view(-1, self.batch_size), hid_state_word)
+            _s, state_word = self.wordRNN(torch.LongTensor(r1).view(-1, self.batch_size), hid_state_word)
             if (s is None):
                 s = _s
             else:
@@ -149,6 +149,101 @@ class SentenceRNN(nn.Module):
 
     def init_hidden_word(self):
         return Variable(torch.zeros(2, self.batch_size, self.hid_size))
+
+
+def train_data(batch_size, review, targets, sent_attn_model, sent_optimizer, criterion):
+    state_word = sent_attn_model.init_hidden_word()
+    state_sent = sent_attn_model.init_hidden_sent()
+    sent_optimizer.zero_grad()
+
+    y_pred, state_sent = sent_attn_model(review, state_sent, state_word)
+
+    loss = criterion(y_pred, torch.LongTensor(targets))
+
+    max_index = y_pred.max(dim=1)[1]
+    correct = (max_index == torch.LongTensor(targets)).sum()
+    acc = float(correct) / batch_size
+
+    loss.backward()
+
+    sent_optimizer.step()
+
+    return torch.tensor(loss).float(), acc
+
+
+import time
+import math
+
+
+def timeSince(since):
+    now = time.time()
+    s = now - since
+    m = math.floor(s / 60)
+    s -= m * 60
+    return '%dm %ds' % (m, s)
+
+
+def gen_batch(x, y, batch_size):
+    k = random.sample(range(len(x) - 1), batch_size)
+    x_batch = []
+    y_batch = []
+
+    for t in k:
+        x_batch.append(x[t])
+        y_batch.append(y[t])
+
+    return [x_batch, y_batch]
+
+
+def validation_accuracy(batch_size, x_val, y_val, sent_attn_model):
+    acc = []
+    val_length = len(x_val)
+    for j in range(int(val_length / batch_size)):
+        x, y = gen_batch(x_val, y_val, batch_size)
+        state_word = sent_attn_model.init_hidden_word()
+        state_sent = sent_attn_model.init_hidden_sent()
+
+        y_pred, state_sent = sent_attn_model(x, state_sent, state_word)
+        max_index = y_pred.max(dim=1)[1]
+        correct = (max_index == torch.LongTensor(y)).sum()
+        acc.append(float(correct) / batch_size)
+    return np.mean(acc)
+
+
+def train_early_stopping(batch_size, x_train, y_train, x_val, y_val, sent_attn_model,
+                         sent_attn_optimiser, loss_criterion, num_epoch,
+                         print_loss_every=50, code_test=True):
+    start = time.time()
+    loss_full = []
+    loss_epoch = []
+    acc_epoch = []
+    acc_full = []
+    val_acc = []
+    epoch_counter = 0
+    train_length = len(x_train)
+    for i in range(1, num_epoch + 1):
+        loss_epoch = []
+        acc_epoch = []
+        for j in range(int(train_length / batch_size)):
+            x, y = gen_batch(x_train, y_train, batch_size)
+            loss, acc = train_data(batch_size, x, y, sent_attn_model, sent_attn_optimiser, loss_criterion)
+            loss_epoch.append(loss)
+            acc_epoch.append(acc)
+            if (code_test and j % int(print_loss_every / batch_size) == 0):
+                print('Loss at %d paragraphs, %d epoch,(%s) is %f' % (
+                    j * batch_size, i, timeSince(start), np.mean(loss_epoch)))
+                print('Accuracy at %d paragraphs, %d epoch,(%s) is %f' % (
+                    j * batch_size, i, timeSince(start), np.mean(acc_epoch)))
+
+        loss_full.append(np.mean(loss_epoch))
+        acc_full.append(np.mean(acc_epoch))
+        torch.save(sent_attn_model.state_dict(), 'sent_attn_model_yelp.pth')
+        print('Loss after %d epoch,(%s) is %f' % (i, timeSince(start), np.mean(loss_epoch)))
+        print('Train Accuracy after %d epoch,(%s) is %f' % (i, timeSince(start), np.mean(acc_epoch)))
+
+        val_acc.append(validation_accuracy(batch_size, x_val, y_val, sent_attn_model))
+        print('Validation Accuracy after %d epoch,(%s) is %f' % (i, timeSince(start), val_acc[-1]))
+    return loss_full, acc_full, val_acc
 
 
 if __name__ == '__main__':
@@ -174,8 +269,12 @@ if __name__ == '__main__':
     test = df[train_len + val_len:]
 
     ## Fix the maximum length of sentences in a paragraph and words in a sentence
-    max_sent_len = 12  # max length of a sentence
-    max_seq_len = 25  # max length of sentences in a paragraph
+    # max_sent_len = 12  # max length of a sentence
+    # max_seq_len = 25  # max length of sentences in a paragraph
+
+    max_sent_len = 5  # max length of a sentence
+    max_seq_len = 10  # max length of sentences in a paragraph
+
 
     ## divides review in sentences and sentences into word creating a 3DList
     x_train = create3DList(train, col_text, max_sent_len, max_seq_len)
@@ -253,3 +352,26 @@ if __name__ == '__main__':
     X_train_pad = [sub_list + [[0]] * (max_sent_len - len(sub_list)) for sub_list in x_train_vec]
     X_val_pad = [sub_list + [[0]] * (max_sent_len - len(sub_list)) for sub_list in x_val_vec]
     X_test_pad = [sub_list + [[0]] * (max_sent_len - len(sub_list)) for sub_list in x_test_vec]
+
+    print(len(X_train_pad), len(X_val_pad), len(X_test_pad))
+    batch_size = 1
+
+    hid_size = 100
+    embedsize = 200
+
+    sent_attn = SentenceRNN(vocab_size, embedsize, batch_size, hid_size, classes)
+    # sent_attn.cuda()
+    sent_attn.wordRNN.embed.from_pretrained(weights)
+    torch.backends.cudnn.benchmark = False
+
+    learning_rate = 1e-3
+    momentum = 0.9
+
+    sent_optimizer = torch.optim.SGD(sent_attn.parameters(), lr=learning_rate, momentum=momentum)
+
+    criterion = nn.NLLLoss()
+    epoch = 200
+
+    loss_full, acc_full, val_acc = train_early_stopping(batch_size, X_train_pad, y_train_tensor, X_val_pad,
+                                                        y_val_tensor, sent_attn, sent_optimizer, criterion, epoch,
+                                                        10000, False)
