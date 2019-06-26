070679ad6c86629cae59669996bc51b3e0ba0293
hvdthong <hvdthong@gmail.com>
2019-06-26 21:33:56 +0800

    add files

--- main.py
+++ main.py
@@ -129,7 +129,7 @@ class SentenceRNN(nn.Module):
                 else:
                     r.append(inp[j][i])
             r1 = np.asarray([sub_list + [0] * (max_seq_len - len(sub_list)) for sub_list in r])
-            _s, state_word = self.wordRNN(torch.LongTensor(r1).view(-1, self.batch_size), hid_state_word)
+            _s, state_word = self.wordRNN(torch.cuda.LongTensor(r1).view(-1, batch_size), hid_state_word)
             if (s is None):
                 s = _s
             else:
@@ -145,10 +145,10 @@ class SentenceRNN(nn.Module):
         return cls, hid_state
 
     def init_hidden_sent(self):
-        return Variable(torch.zeros(2, self.batch_size, self.hid_size))
+        return Variable(torch.zeros(2, self.batch_size, self.hid_size)).cuda()
 
     def init_hidden_word(self):
-        return Variable(torch.zeros(2, self.batch_size, self.hid_size))
+        return Variable(torch.zeros(2, self.batch_size, self.hid_size)).cuda()
 
 
 def train_data(batch_size, review, targets, sent_attn_model, sent_optimizer, criterion):
@@ -158,17 +158,18 @@ def train_data(batch_size, review, targets, sent_attn_model, sent_optimizer, cri
 
     y_pred, state_sent = sent_attn_model(review, state_sent, state_word)
 
-    loss = criterion(y_pred, torch.LongTensor(targets))
+    loss = criterion(y_pred.cuda(), torch.cuda.LongTensor(targets))
 
     max_index = y_pred.max(dim=1)[1]
-    correct = (max_index == torch.LongTensor(targets)).sum()
+    correct = (max_index == torch.cuda.LongTensor(targets)).sum()
     acc = float(correct) / batch_size
 
     loss.backward()
 
     sent_optimizer.step()
 
-    return torch.tensor(loss).float(), acc
+    #     return torch.tensor(loss).float(),acc
+    return loss.clone().detach(), acc
 
 
 import time
@@ -205,7 +206,7 @@ def validation_accuracy(batch_size, x_val, y_val, sent_attn_model):
 
         y_pred, state_sent = sent_attn_model(x, state_sent, state_word)
         max_index = y_pred.max(dim=1)[1]
-        correct = (max_index == torch.LongTensor(y)).sum()
+        correct = (max_index == torch.cuda.LongTensor(y)).sum()
         acc.append(float(correct) / batch_size)
     return np.mean(acc)
 
@@ -215,11 +216,8 @@ def train_early_stopping(batch_size, x_train, y_train, x_val, y_val, sent_attn_m
                          print_loss_every=50, code_test=True):
     start = time.time()
     loss_full = []
-    loss_epoch = []
-    acc_epoch = []
     acc_full = []
     val_acc = []
-    epoch_counter = 0
     train_length = len(x_train)
     for i in range(1, num_epoch + 1):
         loss_epoch = []
@@ -227,13 +225,13 @@ def train_early_stopping(batch_size, x_train, y_train, x_val, y_val, sent_attn_m
         for j in range(int(train_length / batch_size)):
             x, y = gen_batch(x_train, y_train, batch_size)
             loss, acc = train_data(batch_size, x, y, sent_attn_model, sent_attn_optimiser, loss_criterion)
-            loss_epoch.append(loss)
+            loss_epoch.append(loss.item())
             acc_epoch.append(acc)
-            if (code_test and j % int(print_loss_every / batch_size) == 0):
-                print('Loss at %d paragraphs, %d epoch,(%s) is %f' % (
-                    j * batch_size, i, timeSince(start), np.mean(loss_epoch)))
-                print('Accuracy at %d paragraphs, %d epoch,(%s) is %f' % (
-                    j * batch_size, i, timeSince(start), np.mean(acc_epoch)))
+            if code_test and j % int(print_loss_every / batch_size) == 0:
+                print('Loss at %d paragraphs, %d epoch,(%s) is %f' % (j * batch_size, i, timeSince(start),
+                                                                      np.mean(loss_epoch)))
+                print('Accuracy at %d paragraphs, %d epoch,(%s) is %f' % (j * batch_size, i, timeSince(start),
+                                                                          np.mean(acc_epoch)))
 
         loss_full.append(np.mean(loss_epoch))
         acc_full.append(np.mean(acc_epoch))
@@ -263,6 +261,7 @@ def test_accuracy(batch_size, x_test, y_test, sent_attn_model):
 
 if __name__ == '__main__':
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print('Device:', device)
     df = pd.read_csv('yelp.csv')
     print(df.head())
 
@@ -289,7 +288,6 @@ if __name__ == '__main__':
 
     max_sent_len = 5  # max length of a sentence
     max_seq_len = 10  # max length of sentences in a paragraph
-
 
     ## divides review in sentences and sentences into word creating a 3DList
     x_train = create3DList(train, col_text, max_sent_len, max_seq_len)
@@ -345,7 +343,7 @@ if __name__ == '__main__':
                    for text in texts] for texts in x_test_texts]
     x_val_vec = [[[word2vec.wv.vocab[token].index for token in text]
                   for text in texts] for texts in x_val_texts]
-    weights = torch.FloatTensor(word2vec.wv.syn0)
+    weights = torch.FloatTensor(word2vec.wv.vectors)
     print('Shape of word vectors: ', weights.shape)
 
     vocab_size = len(word2vec.wv.vocab)
@@ -361,23 +359,23 @@ if __name__ == '__main__':
 
     max_seq_len = max([len(seq) for seq in itertools.chain.from_iterable(x_train_vec + x_val_vec + x_test_vec)])
     max_sent_len = max([len(sent) for sent in (x_train_vec + x_val_vec + x_test_vec)])
-    print(max_seq_len, max_sent_len)
+    # print(max_seq_len, max_sent_len)  # max sequence of our data, max length of our data
 
     ## Padding the input
     X_train_pad = [sub_list + [[0]] * (max_sent_len - len(sub_list)) for sub_list in x_train_vec]
     X_val_pad = [sub_list + [[0]] * (max_sent_len - len(sub_list)) for sub_list in x_val_vec]
     X_test_pad = [sub_list + [[0]] * (max_sent_len - len(sub_list)) for sub_list in x_test_vec]
 
-    print(len(X_train_pad), len(X_val_pad), len(X_test_pad))
-    batch_size = 1
+    # print(len(X_train_pad), len(X_val_pad), len(X_test_pad))
+    batch_size = 64
 
     hid_size = 100
     embedsize = 200
 
     sent_attn = SentenceRNN(vocab_size, embedsize, batch_size, hid_size, classes)
-    # sent_attn.cuda()
+    sent_attn.cuda()
     sent_attn.wordRNN.embed.from_pretrained(weights)
-    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.benchmark = True
 
     learning_rate = 1e-3
     momentum = 0.9
@@ -385,8 +383,9 @@ if __name__ == '__main__':
     sent_optimizer = torch.optim.SGD(sent_attn.parameters(), lr=learning_rate, momentum=momentum)
 
     criterion = nn.NLLLoss()
-    epoch = 10
+    epoch = 200
 
+    print('Training data-----')
     loss_full, acc_full, val_acc = train_early_stopping(batch_size, X_train_pad, y_train_tensor, X_val_pad,
                                                         y_val_tensor, sent_attn, sent_optimizer, criterion, epoch,
                                                         10000, False)
@@ -406,5 +405,4 @@ if __name__ == '__main__':
     plt.ylabel('Validation Accuracy')
     plt.xlabel('Epoch')
     plt.savefig('val_acc.png')
-
-    test_accuracy(batch_size, X_test_pad, y_test_tensor, sent_attn)
+    print('Final accuracy after training:', test_accuracy(batch_size, X_test_pad, y_test_tensor, sent_attn))
